740ad939ec934493d4b67ace1419f6e71dc369b6
v_vinh <tranvinh169@GMAIL.COM>
2015-09-08 17:07:42 +0700

    [2015/09/08 : 17:07] vvinh : Spring MyBatis add

---
+++ src/main/java/com/faceme/controller/StudentController.java
@@ -0,0 +1,56 @@
+package com.faceme.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.faceme.model.Student;
+import com.faceme.service.StudentService;
+
+@Controller
+@SessionAttributes("student")
+public class StudentController {
+	
+	@Autowired
+	private StudentService studentService;
+	
+	@RequestMapping(value="/signup", method=RequestMethod.GET)
+	public String signup(Model model) {
+		Student student = new Student();
+		model.addAttribute("student", student);
+		return "signup";
+	}
+	
+	@RequestMapping(value="/signup", method=RequestMethod.POST)
+	public String signup(@ModelAttribute("student") Student student, Model model) {
+		if(studentService.getStudentByUserName(student.getUserName())) {
+			model.addAttribute("message", "User Name exists. Try another user name");
+			return "signup";
+		} else {
+			studentService.insertStudent(student);
+			model.addAttribute("message", "Saved student details");
+			return "redirect:login.html";
+		}
+	}
+	
+	@RequestMapping(value="/login", method=RequestMethod.GET)
+	public String login(Model model) {
+		Student studentLogin = new Student();
+		model.addAttribute("studentLogin", studentLogin);
+		return "login";
+	}
+	
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	public String login(@ModelAttribute("studentLogin") Student studentLogin) {
+		boolean found = studentService.getStudentByLogin(studentLogin.getUserName(), studentLogin.getPassword());
+		if (found) {				
+			return "success";
+		} else {				
+			return "failure";
+		}
+	}
+}
---
+++ src/main/java/com/faceme/service/StudentService.java
@@ -0,0 +1,9 @@
+package com.faceme.service;
+
+import com.faceme.model.Student;
+
+public interface StudentService {
+	void insertStudent(Student student);
+	boolean getStudentByLogin(String userName, String password);
+	boolean getStudentByUserName(String userName);
+}
